8d40ece2eae0b860297d30d71728c53aae84ed6b
mhhasanmehedi <mmmehedi53@gmail.com>
2022-04-21 14:29:34 +0600

    card-starter

---
+++ src/components/Cards/Cards.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { CardData } from '../../Data/Data';
+import Card from '../Card/Card';
+import './Cards.css';
+
+const Cards = () => {
+  return (
+    <div className='cards'>
+      {CardData.map((card, id) => (
+        <div className='parentContainter'>
+          <Card
+            title={card.title}
+            color={card.color}
+            barValue={card.barValue}
+            value={card.value}
+            png={card.png}
+            series={card.series}
+          />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Cards;
